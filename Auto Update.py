8a5634c39ea7346eedc446acbcf64cca1cec919f
pascaldercoole1 <83749737+pascaldercoole1@users.noreply.github.com>
2023-07-04 06:51:30 +0200

    Update Auto Update.py

--- Auto Update.py
+++ Auto Update.py
@@ -8,6 +8,12 @@ lokaler_pfad = os.path.expanduser("~/Roblox-Login")
 # GitHub-Repository-URL
 repository_url = "https://github.com/pascaldercoole1/Roblox-Login/raw/main/Roblox%20Cookie%20Login.py"
 
+print("Searching for Updates...")
+
+time.sleep(1)
+
+print("Downloading latest Version...")
+
 # Datei herunterladen
 response = requests.get(repository_url)
 datei_name = os.path.join(lokaler_pfad, "Roblox Cookie Login.py")
@@ -20,6 +26,9 @@ with open(datei_name, "wb") as f:
     f.write(response.content)
 
 # Die heruntergeladene Datei öffnen
+
+print("Staring latest Version...")
+
 os.startfile(datei_name)
 
 time.sleep(3)
