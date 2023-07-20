3df9261d6d9479484165152f8e725a70717eab38
pascaldercoole1 <83749737+pascaldercoole1@users.noreply.github.com>
2023-07-20 23:26:26 +0200

    Create AutoInstall_Requirements.py

---
+++ AutoInstall_Requirements.py
@@ -0,0 +1,7 @@
+import subprocess
+subprocess.check_call(["pip", "install", "webbrowser"])
+subprocess.check_call(["pip", "install", "time"])
+subprocess.check_call(["pip", "install", "json"])
+subprocess.check_call(["pip", "install", "psutil"])
+subprocess.check_call(["pip", "install", "pathlib"])
+subprocess.check_call(["pip", "install", "selenium"])
