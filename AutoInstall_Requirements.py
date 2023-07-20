d53ce742c13acaebe1ec69442d394f82a88e03ee
pascaldercoole1 <83749737+pascaldercoole1@users.noreply.github.com>
2023-07-20 23:27:55 +0200

    Delete AutoInstall_Requirements.py

--- AutoInstall_Requirements.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-subprocess.check_call(["pip", "install", "webbrowser"])
-subprocess.check_call(["pip", "install", "time"])
-subprocess.check_call(["pip", "install", "json"])
-subprocess.check_call(["pip", "install", "psutil"])
-subprocess.check_call(["pip", "install", "pathlib"])
-subprocess.check_call(["pip", "install", "selenium"])
